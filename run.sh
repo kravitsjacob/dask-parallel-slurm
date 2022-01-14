75b630a876713e402e983ae0246cdbad7588cb13
kravitsjacob <kravitsjacob@gmail.com>
2022-01-14 15:28:40 -0700

    Update command lines arguments to only required

--- run.sh
+++ run.sh
@@ -22,8 +22,6 @@ conda activate dask-parallel
 python main.py \
   --npartitions=2 \
   --n_workers=2 \
-  --worker_queue=amilan-ucb \
   --worker_cores=1 \
-  --worker_memory='1 GB' \
+  --worker_memory='0.5 GB' \
   --worker_processes=1 \
-  --worker_walltime=00:10:00 \
