611912d02da9414938bf1b2a0d2d3e069dc9f429
kravitsjacob <kravitsjacob@gmail.com>
2022-01-14 15:05:42 -0700

    add command line arguments to slurm batch

--- run.sh
+++ run.sh
@@ -21,4 +21,11 @@ conda env create -f environment.yml
 conda activate dask-parallel
 
 # Run
-python main.py
+python main.py \
+  --npartitions=2 \
+  --n_workers=2 \
+  --worker_queue=amilan-ucb \
+  --worker_cores=1 \
+  --worker_memory='1 GB' \
+  --worker_processes=1 \
+  --worker_walltime=00:10:00 \
