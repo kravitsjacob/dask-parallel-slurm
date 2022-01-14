396f9a752e235df8a33c05d5ae92d7ae83440a3a
Jacob Kravits <kravitsjacob@gmail.com>
2022-01-14 10:10:09 -0700

    Change 'job' terminology to simulation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# from dask_jobqueue import SLURMCluster
-# from dask.distributed import Client, LocalCluster, progress
-# import dask.dataframe as dd
+from dask_jobqueue import SLURMCluster
+from dask.distributed import Client, LocalCluster, progress
+import dask.dataframe as dd
 import numpy as np
 import pandas as pd
 
@@ -8,8 +8,8 @@ NUMNODES = 4
 NUMPROCESSES = 1
 
 
-def job(inputs, constant):
-    """General 'job' to be parallelized which has `inputs` that
+def simulation(inputs, constant):
+    """General 'simulation' to be parallelized which has `inputs` that
     change on every evaluation and a `constant` object that does
     not chaange on every evalation.
 
@@ -46,39 +46,26 @@ def main():
 
     # Run serially on first 5 to test
     res_test = df.iloc[0:5].apply(
-        lambda row: job(inputs=row, constant=1008),
+        lambda row: simulation(inputs=row, constant=1008),
         axis=1
     )
     print('Results of serial test')
     print(res_test)
-    
+
+    # Run parallel simulation
+    cluster = SLURMCluster(cores=2, memory='1 GB', processes=NUMPROCESSES, walltime='00:10:00', queue='amilan-ucb')
+    cluster.scale(n=1)
+    client = Client(cluster)
+    ddf = dd.from_pandas(df, npartitions=10)
+    run = ddf.apply(
+        simulation,
+        axis=1,
+        meta=pd.DataFrame(columns=['Max', 'Min', 'Mean'], dtype='float64')
+    ).persist()
+    progress(run)
+    res = run.compute(scheduler=client)
+    print(res)
 
 
 if __name__ == '__main__':
     main()
-    # cluster = SLURMCluster(cores=2, memory='1 GB', processes=NUMPROCESSES, walltime='00:10:00', queue='amilan-ucb')
-    # cluster.scale(NUMNODES*NUMPROCESSES)
-    # client = Client(cluster)
-    # print("start script...")
-    # x = np.random.normal(0, 1, (100, 5))
-    # df = pd.DataFrame(
-    #     columns=['Input 1', 'Input 2', 'Input 3', 'Input 4', 'Input 5'],
-    #     data=np.random.normal(0, 1, (100, 5))
-    # )
-    # a = df.apply(
-    #     lambda row: job(inputs=row, constant=1008),
-    #     axis=1
-    # )
-    # print(a)
-
-
-    # ddf = dd.from_pandas(df, npartitions=10)
-    # run = ddf.apply(
-    #     job,
-    #     axis=1,
-    #     meta=pd.DataFrame(columns=['Max', 'Min', 'Mean'], dtype='float64')
-    # ).persist()
-    # progress(run)
-    # res = run.compute(scheduler=client)
-    # print(res)
-    # print("finished!")
